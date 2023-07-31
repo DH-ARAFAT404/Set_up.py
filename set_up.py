f75e834a728e6e5de158a7445c51f8cbaab22b23
DH-ARAFAT404 <140757646+DH-ARAFAT404@users.noreply.github.com>
2023-07-31 16:20:12 +0600

    Set_up.py

---
+++ set_up.py
@@ -0,0 +1,31 @@
+
+import os
+
+
+#Install apt
+os.system('apt update && apt upgrade')
+
+#Install all command
+os.system('pip install httpx')
+os.system('pip install beautifulsoup')
+os.system('apt update')
+os.system('apt upgrade')
+os.system('termux-setup-storage')
+os.system('pkg update')
+os.system('pkg upgrade')
+os.system('pkg install python')
+os.system('pkg install git')
+os.system('pkg install ruby')
+os.system('gem install lolcat')
+os.system('pip install requests')
+os.system('pip install mechanize')
+os.system('pip install bs4')
+os.system('clear')
+
+
+print(' \033[1;34mㄚ龱ㄩ尺 丂㠪ㄒ-ㄩ尸 工丂 ᗪ龱𝓝㠪⸝⸝⸝...\033[1;32m闩ᗪ爪工𝓝 :: 闩尺闩千闩ㄒ ')
+print('\033[1;34m=================================================')                                
+print('\033[1;34m--------------------------------------------------------------------------------------------------')
+print('\033[1;34mFacebook :: AR AF AT ..........')
+print('\033[1;34mWhatsapp :: 01749953660...')
+print('\033[1;34m Author :: EYASIN ARAFAT')
